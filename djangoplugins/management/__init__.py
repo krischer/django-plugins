845651d5b8432069acb8f4549189264c08e3b8fd
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-01-18 17:04:31 +0100

    Call the syncdb command also for Django 1.9

--- djangoplugins/management/__init__.py
+++ djangoplugins/management/__init__.py
@@ -1,14 +1,13 @@
 from __future__ import absolute_import
 
+# Import cascade to work with a number of Django versions.
 try:
     from django.db.models.signals import post_migrate
 except ImportError:
-    from south.signals import post_migrate
-
-try:
-    from django.db.models.signals import post_syncdb
-except ImportError:
-    from django.db.models.signals import post_migrate as post_syncdb
+    try:
+        from django.db.models.signals import post_syncdb as post_migrate
+    except ImportError:
+        from south.signals import post_migrate
 
 
 from djangoplugins import models as plugins_app
@@ -16,11 +15,11 @@ from .commands.syncplugins import SyncPlugins
 
 
 def sync_plugins(sender, verbosity, **kwargs):
-    SyncPlugins(False, verbosity).all()
+    # Different django version have different senders.
+    if (hasattr(sender, "name") and sender.name == "djangoplugins") or \
+            (sender == plugins_app):
+        SyncPlugins(False, verbosity).all()
 
-# Always run after `migrate`.
-post_migrate.connect(sync_plugins, sender=plugins_app)
 
-# This is somehow needed for the tests to work.
-# XXX: Figure out how to get rid of this.
-post_syncdb.connect(sync_plugins, sender=plugins_app)
+# Plugins must be synced to the database.
+post_migrate.connect(sync_plugins)
